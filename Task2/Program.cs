d2f617689715ae19cedab60d1ec1271a40d91b73
VasilySerkov <serkov_v@mail.ru>
2022-08-19 21:39:23 +0300

    Добавлены задачи 2, 4, 6, 8

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,11 +4,13 @@ Console.WriteLine("Введите число b: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 if(numberA > numberB)   
 {
+Console.Write("max=");
 Console.WriteLine(numberA);
  }
 else
     if(numberA < numberB)
     {
+    Console.Write("max=");
     Console.WriteLine(numberB);
     }
     else
---
+++ Task8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while (count <= number)
+{
+    Console.WriteLine(count);
+    count = count + 2;
+}
