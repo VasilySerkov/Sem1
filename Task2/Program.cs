2db9046ac3f1db233afcb7108024c48e838497a7
VasilySerkov <serkov_v@mail.ru>
2022-08-19 20:33:11 +0300

    задание 2 и 4

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число a: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA > numberB)   
+{
+Console.WriteLine(numberA);
+ }
+else
+    if(numberA < numberB)
+    {
+    Console.WriteLine(numberB);
+    }
+    else
+    {
+    Console.WriteLine("Эти числа равны");
+    }
---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число a: ");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+if (max < numberB)
+{
+max = numberB;
+}
+if (max < numberC)
+{
+max = numberC;
+}
+Console.Write("max=");
+Console.WriteLine(max);
