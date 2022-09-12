b794460168a7334669190ed762e9ec961c636f7e
VasilySerkov <serkov_v@mail.ru>
2022-09-12 22:16:38 +0300

    Добавлены задачи 64, 66 и 68

---
+++ Task66/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число M:");
+int M = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число N, больше M:");
+int N = int.Parse(Console.ReadLine());
+
+void GapSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    GapSum(numberM, numberN, sum);
+}
+
+GapSum(M, N, 0);
---
+++ Task68/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число m:");
+int M = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число n:");
+int N = int.Parse(Console.ReadLine());
+
+int AckermannFunction (int M, int N)
+{
+    if (M == 0) return N + 1;
+    if (M != 0 && N == 0) return AckermannFunction(M - 1, 1);
+    if (M > 0 && N > 0) return AckermannFunction(M - 1, AckermannFunction(M, N - 1));
+return AckermannFunction(M, N);
+}
+
+Console.WriteLine($"Функция Аккермана для чисел A({M},{N}) = {AckermannFunction(M, N)}");
